afde5af6d200aa0dd89cecd11ab21c8537d6229a
Zair <zair@mobilityps.com>
2021-08-14 18:41:36 -0300

    Encapsulamiento, set & get

--- TP1/src/Ejercicio1/Empleado.java
+++ TP1/src/Ejercicio1/Empleado.java
@@ -1,7 +1,29 @@
 package Ejercicio1;
 
 public class Empleado {
-	int id;
-	String nombre;
-	int edad;
+	
+	private int id;
+	private String nombre;
+	private int edad;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
 }
