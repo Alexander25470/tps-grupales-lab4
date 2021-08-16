1fa8281a5045c6c85d6a575634cb98f1b782110e
NahuelGodoy <64289121+Jogana3@users.noreply.github.com>
2021-08-16 16:21:15 -0300

    Id de empleado creado, constructor vacio y con parametros creados tambien

--- TP1/src/Ejercicio1/Empleado.java
+++ TP1/src/Ejercicio1/Empleado.java
@@ -2,9 +2,28 @@ package Ejercicio1;
 
 public class Empleado {
 	
-	private int id;
+	private final int id;
 	private String nombre;
 	private int edad;
+	private static int num_id = 1000;
+	
+	public Empleado() {
+		
+		this.id = num_id;
+		this.nombre = "Sin nombre";
+		this.edad = 99;
+		
+		num_id++;
+	}
+	
+	public Empleado(String nombre, int edad) {
+		
+		this.id = num_id;
+		this.nombre = nombre;
+		this.edad = edad;
+		
+		num_id++;
+	}
 	
 	public int getId() {
 		return id;
