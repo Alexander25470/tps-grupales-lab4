4e252eb6e8bc2d1d29d2cc9bbade94f41150e0c3
Alexander Popp <Alexander.Popp@Sarsy-srl.com>
2021-09-25 19:25:30 -0300

    ej 1, y parte visual ej 2

--- TP4_Grupo_1/src/Menu/Ejercicio1.java
+++ TP4_Grupo_1/src/Menu/Ejercicio1.java
@@ -1,11 +1,17 @@
 package Menu;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ejercicio1 extends JFrame {
 
@@ -14,6 +20,13 @@ public class Ejercicio1 extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textFieldNombre;
+	private JTextField textFieldApellido;
+	private JTextField textFieldTelefono;
+	private JTextField textFieldFechaNac;
+	private JButton btnMostrar;
+	private JLabel lblLosDatosIngresados;
+	private JLabel lblResultado;
 
 	/**
 	 * Create the frame.
@@ -24,8 +37,91 @@ public class Ejercicio1 extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(31, 54, 46, 14);
+		contentPane.add(lblNombre);
+		
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(31, 90, 46, 14);
+		contentPane.add(lblApellido);
+		
+		JLabel lblTelefono = new JLabel("Telefono");
+		lblTelefono.setBounds(31, 127, 46, 14);
+		contentPane.add(lblTelefono);
+		
+		JLabel lblFechaNac = new JLabel("Fecha nac.");
+		lblFechaNac.setBounds(31, 164, 65, 14);
+		contentPane.add(lblFechaNac);
+		
+		textFieldNombre = new JTextField();
+		textFieldNombre.setBounds(160, 51, 86, 20);
+		contentPane.add(textFieldNombre);
+		textFieldNombre.setColumns(10);
+		
+		textFieldApellido = new JTextField();
+		textFieldApellido.setBounds(160, 87, 86, 20);
+		contentPane.add(textFieldApellido);
+		textFieldApellido.setColumns(10);
+		
+		textFieldTelefono = new JTextField();
+		textFieldTelefono.setBounds(160, 124, 86, 20);
+		contentPane.add(textFieldTelefono);
+		textFieldTelefono.setColumns(10);
+		
+		textFieldFechaNac = new JTextField();
+		textFieldFechaNac.setBounds(160, 161, 86, 20);
+		contentPane.add(textFieldFechaNac);
+		textFieldFechaNac.setColumns(10);
+		
+		lblLosDatosIngresados = new JLabel("Los datos ingresados fueron:");
+		lblLosDatosIngresados.setBounds(10, 217, 236, 14);
+		contentPane.add(lblLosDatosIngresados);
+		
+		lblResultado = new JLabel("Resultado");
+		lblResultado.setBounds(10, 242, 414, 14);
+		contentPane.add(lblResultado);
+		
+		btnMostrar = new JButton("Mostrar");
+		btnMostrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				boolean hayCamposVacios = false;
+				if(textFieldNombre.getText().isEmpty()) {
+					textFieldNombre.setBackground(Color.red);
+					hayCamposVacios = true;
+				}else{
+					textFieldNombre.setBackground(Color.WHITE);
+				}
+				if(textFieldApellido.getText().isEmpty()) {
+					textFieldApellido.setBackground(Color.red);
+					hayCamposVacios = true;
+				}else {
+					textFieldApellido.setBackground(Color.WHITE);
+				}
+				if(textFieldTelefono.getText().isEmpty()) {
+					textFieldTelefono.setBackground(Color.red);
+					hayCamposVacios = true;
+				}else{
+					textFieldTelefono.setBackground(Color.WHITE);
+				}
+				if(textFieldFechaNac.getText().isEmpty()) {
+					textFieldFechaNac.setBackground(Color.red);
+					hayCamposVacios = true;
+				}else{
+					textFieldFechaNac.setBackground(Color.WHITE);
+				}
+				if(hayCamposVacios) {
+					lblResultado.setText("Complete todos los campos");
+					return;
+				}
+				lblResultado.setText( textFieldNombre.getText() + " " + textFieldApellido.getText() + " " + textFieldTelefono.getText() + " " + textFieldFechaNac.getText());
+			}
+		});
+		btnMostrar.setBounds(251, 191, 89, 23);
+		contentPane.add(btnMostrar);
+		
+		
 	}
-
 }
--- TP4_Grupo_1/src/Menu/Ejercicio2.java
+++ TP4_Grupo_1/src/Menu/Ejercicio2.java
@@ -6,6 +6,25 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.JSeparator;
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.GridLayout;
+import net.miginfocom.swing.MigLayout;
+import java.awt.GridBagLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.Box;
+import java.awt.Panel;
+import javax.swing.border.TitledBorder;
+import javax.swing.JComboBox;
 
 public class Ejercicio2 extends JFrame {
 
@@ -14,6 +33,11 @@ public class Ejercicio2 extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Create the frame.
@@ -24,8 +48,84 @@ public class Ejercicio2 extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Notas del estudiante", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(34, 11, 198, 140);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNota = new JLabel("Nota 1:");
+		lblNota.setBounds(10, 23, 46, 14);
+		panel.add(lblNota);
+		
+		JLabel lblNota_1 = new JLabel("Nota 2:");
+		lblNota_1.setBounds(10, 48, 46, 14);
+		panel.add(lblNota_1);
+		
+		JLabel lblNota_2 = new JLabel("Nota 3:");
+		lblNota_2.setBounds(10, 73, 46, 14);
+		panel.add(lblNota_2);
+		
+		JLabel lblTps = new JLabel("TPS");
+		lblTps.setBounds(10, 98, 46, 14);
+		panel.add(lblTps);
+		
+		textField = new JTextField();
+		textField.setBounds(102, 20, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(102, 45, 86, 20);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(102, 70, 86, 20);
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(102, 95, 86, 20);
+		panel.add(comboBox);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(null, "Notas del estudiante", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBounds(34, 162, 198, 88);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblPromedio = new JLabel("Promedio:");
+		lblPromedio.setBounds(10, 31, 60, 14);
+		panel_1.add(lblPromedio);
+		
+		JLabel lblCondicion = new JLabel("Condicion:");
+		lblCondicion.setBounds(10, 56, 60, 14);
+		panel_1.add(lblCondicion);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(102, 28, 86, 20);
+		panel_1.add(textField_4);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(102, 53, 86, 20);
+		panel_1.add(textField_5);
+		textField_5.setColumns(10);
+		
+		JButton btnCalcular = new JButton("Calcular");
+		btnCalcular.setBounds(302, 56, 89, 23);
+		contentPane.add(btnCalcular);
+		
+		JButton btnNuevo = new JButton("Nuevo");
+		btnNuevo.setBounds(302, 90, 89, 23);
+		contentPane.add(btnNuevo);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.setBounds(302, 124, 89, 23);
+		contentPane.add(btnSalir);
 	}
-
 }
