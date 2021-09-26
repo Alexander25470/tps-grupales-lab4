b34e529313204d3d777b5d3ea51bc0c5cf82a8f7
Alexander Popp <Alexander.Popp@Sarsy-srl.com>
2021-09-26 16:53:49 -0300

    ej2 y 3 terminado

--- TP4_Grupo_1/src/Menu/Ejercicio2.java
+++ TP4_Grupo_1/src/Menu/Ejercicio2.java
@@ -7,6 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextPane;
 import javax.swing.JSeparator;
 import javax.swing.JButton;
@@ -25,6 +26,8 @@ import javax.swing.Box;
 import java.awt.Panel;
 import javax.swing.border.TitledBorder;
 import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ejercicio2 extends JFrame {
 
@@ -33,11 +36,11 @@ public class Ejercicio2 extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField textField_nota1;
+	private JTextField textField_nota2;
+	private JTextField textField_nota3;
+	private JTextField textField_promedio;
+	private JTextField textField_Condicion;
 
 	/**
 	 * Create the frame.
@@ -50,10 +53,11 @@ public class Ejercicio2 extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		 
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "Notas del estudiante", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.setBounds(34, 11, 198, 140);
+		panel.setBounds(34, 11, 210, 140);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -73,28 +77,30 @@ public class Ejercicio2 extends JFrame {
 		lblTps.setBounds(10, 98, 46, 14);
 		panel.add(lblTps);
 		
-		textField = new JTextField();
-		textField.setBounds(102, 20, 86, 20);
-		panel.add(textField);
-		textField.setColumns(10);
+		textField_nota1 = new JTextField();
+		textField_nota1.setBounds(102, 20, 98, 20);
+		panel.add(textField_nota1);
+		textField_nota1.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(102, 45, 86, 20);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
+		textField_nota2 = new JTextField();
+		textField_nota2.setBounds(102, 45, 98, 20);
+		panel.add(textField_nota2);
+		textField_nota2.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(102, 70, 86, 20);
-		panel.add(textField_2);
-		textField_2.setColumns(10);
+		textField_nota3 = new JTextField();
+		textField_nota3.setBounds(102, 70, 98, 20);
+		panel.add(textField_nota3);
+		textField_nota3.setColumns(10);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(102, 95, 86, 20);
+		comboBox.setBounds(102, 95, 98, 20);
+		comboBox.addItem("Aprobado");
+		comboBox.addItem("Desaprobado");
 		panel.add(comboBox);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(null, "Notas del estudiante", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_1.setBounds(34, 162, 198, 88);
+		panel_1.setBounds(34, 162, 210, 88);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
@@ -106,25 +112,72 @@ public class Ejercicio2 extends JFrame {
 		lblCondicion.setBounds(10, 56, 60, 14);
 		panel_1.add(lblCondicion);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(102, 28, 86, 20);
-		panel_1.add(textField_4);
-		textField_4.setColumns(10);
+		textField_promedio = new JTextField();
+		textField_promedio.setBounds(102, 28, 98, 20);
+		panel_1.add(textField_promedio);
+		textField_promedio.setColumns(10);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(102, 53, 86, 20);
-		panel_1.add(textField_5);
-		textField_5.setColumns(10);
+		textField_Condicion = new JTextField();
+		textField_Condicion.setBounds(102, 53, 98, 20);
+		panel_1.add(textField_Condicion);
+		textField_Condicion.setColumns(10);
 		
 		JButton btnCalcular = new JButton("Calcular");
+		btnCalcular.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				float nota1,nota2,nota3;
+				
+				try {
+					nota1 = Float.parseFloat(textField_nota1.getText());
+					nota2 = Float.parseFloat(textField_nota2.getText());
+					nota3 = Float.parseFloat(textField_nota3.getText());
+				} catch (Exception e) {
+					System.out.println("Complete los datos correctamente.");
+					JOptionPane.showMessageDialog(null, "Complete los datos correctamente.");
+					return;
+				}
+				
+			
+				float promedio = (nota1 + nota2 + nota3 ) /3;
+				String notaTps = comboBox.getSelectedItem().toString();
+				
+				textField_promedio.setText(promedio+"");
+				
+				if(notaTps == "Desaprobado") {
+					textField_Condicion.setText("Libre");
+				}else if(nota1<6 || nota2<6 || nota3<6){
+					textField_Condicion.setText("Libre");
+				}else if (nota1>=8 && nota2>=8 && nota3>=8) {
+					textField_Condicion.setText("Promocionado");
+				} else {
+					textField_Condicion.setText("Regular");
+				}
+			}
+		});
 		btnCalcular.setBounds(302, 56, 89, 23);
 		contentPane.add(btnCalcular);
 		
 		JButton btnNuevo = new JButton("Nuevo");
+		btnNuevo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField_nota1.setText("");
+				textField_nota2.setText("");
+				textField_nota3.setText("");
+				textField_promedio.setText("");
+				textField_Condicion.setText("");
+				comboBox.setSelectedIndex(0);
+			}
+		});
 		btnNuevo.setBounds(302, 90, 89, 23);
 		contentPane.add(btnNuevo);
 		
 		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
 		btnSalir.setBounds(302, 124, 89, 23);
 		contentPane.add(btnSalir);
 	}
--- TP4_Grupo_1/src/Menu/Ejercicio3.java
+++ TP4_Grupo_1/src/Menu/Ejercicio3.java
@@ -1,11 +1,23 @@
 package Menu;
 
 import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ejercicio3 extends JFrame {
 
@@ -14,18 +26,117 @@ public class Ejercicio3 extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField_horasEnPc;
+	private ButtonGroup grupoBotones;
 
 	/**
 	 * Create the frame.
 	 */
 	public Ejercicio3() {
+		grupoBotones = new ButtonGroup();
 		setTitle("Ejercicio 3");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.setBounds(23, 11, 401, 39);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblElijaUnSistema = new JLabel("Elija un sistema operativo:");
+		lblElijaUnSistema.setBounds(10, 11, 164, 14);
+		panel.add(lblElijaUnSistema);
+		
+		JRadioButton rdbtnWindows = new JRadioButton("Windows");
+		rdbtnWindows.setBounds(180, 7, 84, 23);
+		panel.add(rdbtnWindows);
+		grupoBotones.add(rdbtnWindows);
+		
+		JRadioButton rdbtnMac = new JRadioButton("Mac");
+		rdbtnMac.setBounds(266, 7, 60, 23);
+		grupoBotones.add(rdbtnMac);
+		panel.add(rdbtnMac);
+		
+		JRadioButton rdbtnLinux = new JRadioButton("Linux");
+		rdbtnLinux.setBounds(328, 7, 67, 23);
+		grupoBotones.add(rdbtnLinux);
+		panel.add(rdbtnLinux);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_1.setBounds(23, 62, 401, 95);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblElijaUnaEspecialidad = new JLabel("Elija una especialidad:");
+		lblElijaUnaEspecialidad.setBounds(10, 42, 116, 14);
+		panel_1.add(lblElijaUnaEspecialidad);
+		
+		JCheckBox chckbxProgramacin = new JCheckBox("Programaci\u00F3n");
+		chckbxProgramacin.setBounds(248, 12, 128, 23);
+		panel_1.add(chckbxProgramacin);
+		
+		JCheckBox chckbxAdministracin = new JCheckBox("Administraci\u00F3n");
+		chckbxAdministracin.setBounds(248, 38, 128, 23);
+		panel_1.add(chckbxAdministracin);
+		
+		JCheckBox chckbxDiseoGrafico = new JCheckBox("Dise\u00F1o grafico");
+		chckbxDiseoGrafico.setBounds(248, 64, 128, 23);
+		panel_1.add(chckbxDiseoGrafico);
+		
+		JLabel lblCantidadDeHoras = new JLabel("Cantidad de horas en el computador:");
+		lblCantidadDeHoras.setBounds(23, 168, 190, 14);
+		contentPane.add(lblCantidadDeHoras);
+		
+		textField_horasEnPc = new JTextField();
+		textField_horasEnPc.setBounds(259, 165, 86, 20);
+		contentPane.add(textField_horasEnPc);
+		textField_horasEnPc.setColumns(10);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String sistema = "";
+				String especialidades = "";
+				String horas = "";
+				if(rdbtnWindows.isSelected()) {
+					sistema="Windows - ";
+				}else if(rdbtnMac.isSelected()) {
+					sistema="Mac - ";
+				} else if(rdbtnLinux.isSelected()) {
+					sistema="Linux - ";
+				} else {
+					JOptionPane.showMessageDialog(null, "Seleccione un sistema" );
+					return;
+				}
+				if(chckbxAdministracin.isSelected()) {
+					especialidades+="Administración - ";
+				}
+				if(chckbxProgramacin.isSelected()) {
+					especialidades+="Programación - ";
+				}
+				if(chckbxDiseoGrafico.isSelected()) {
+					especialidades+="Diseño grafico - ";
+				}
+				if(especialidades=="") {
+					JOptionPane.showMessageDialog(null, "Seleccione al menos una especialidad" );
+					return;
+				}
+				if(textField_horasEnPc.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Ingrese un numero de horas" );
+					return;
+				} else {					
+					horas = textField_horasEnPc.getText() + " hs";
+				}
+				JOptionPane.showMessageDialog(null, sistema + especialidades + horas  );
+			}
+		});
+		btnAceptar.setBounds(320, 227, 89, 23);
+		contentPane.add(btnAceptar);
 	}
-
 }
