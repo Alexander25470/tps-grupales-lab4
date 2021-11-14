9e04fc614488698c0d854397d9b00e14845c3a8a
Franco Coronel <55667889+Francoz0ma@users.noreply.github.com>
2021-11-14 00:54:58 -0300

    Agregue todos los inserts que faltaban y cree una seccion de insert drops y deletes

--- TPINT_GRUPO_1_LAB4/bd.sql
+++ TPINT_GRUPO_1_LAB4/bd.sql
@@ -39,6 +39,11 @@ CREATE TABLE DOCENTES(
     foreign key (ID_Localidad) references LOCALIDADES(id)
 );
 
+CREATE TABLE ESTADOSALUMNO(
+	ID int primary key,
+    descripcion varchar(50)
+);
+
 CREATE TABLE ALUMNOS(
 	legajo int primary key AUTO_INCREMENT,
     dni varchar(12) unique,
@@ -72,10 +77,12 @@ CREATE TABLE NOTAS(
     parcial2 float,
     recuperatorio1 float,
     recuperatorio2 float,
-    estado varchar(50),
+    estado int,
     foreign key (legajo) references ALUMNOS(legajo),
-    foreign key (ID_Curso) references CURSOS(id)
+    foreign key (ID_Curso) references CURSOS(id),
+	foreign key (estado) references ESTADOSALUMNO(ID)
 );
+
 
 CREATE TABLE USUARIOS(
     id int primary key AUTO_INCREMENT,
@@ -101,9 +108,39 @@ insert into LOCALIDADES
 VALUES('1','Tigre'),('2','San Isidro'),('3','Malvinas Argentinas'),
 ('4','Moron'),('5','La Matanza'),('6','Garin'),('7','La Plata');
 
+insert into MATERIAS
+(id,nombre)
+VALUES(1,'Matematica'),('2','Programacion'),('3','Ingles'),
+(4,'Laboratorio'),('5','Legislacion'),('6','Metodologia de sistemas');
 
+insert into docentes
+(dni,nombreApellido,fechaNac,ID_Nacionalidad,ID_Localidad,direccion,email,telefono,estado)
+VALUES('33859632','Maria Perez','1992/10/02','1','3','Av.Libertador 1050','maria@gmail.com','1169325452',1),
+('30506231','Ernesto Fernandez','1990/08/10','5','5','8 de Octubre 3058','ErnestoFe@hotmail.com','1178321052',1);
 
+insert into alumnos
+(dni,nombreApellido,fechaNac,ID_Nacionalidad,ID_Provincia,direccion,email,telefono,estado)
+VALUES('42456321','Lautaro Farias','2000/01/15','2','3','San Martin 1595','lautaro@gmail.com','1178412136',1),
+('40963215','Estela Ricartez','1998/05/15','5','4','Juramento 5050','ricartez@outlook.com','1112457896',1);
 
+insert into estadosalumno
+(id,descripcion)
+VALUES (1,'regular'),(2,'libre');	
+
+insert into cursos
+(ID_Materia,legajo,semestre,anio)
+VALUES('1','1','1',2021),
+('2','2','1',2021);
+
+insert into NOTAS 
+(legajo,ID_Curso,parcial1,parcial2,recuperatorio1,recuperatorio2,estado)
+VALUES(1,1,10,8,null,null,1),
+(2,2,5,5,7,8,1);
+
+insert into USUARIOS
+(legajo_docente,nombre,contrasenia,administrador)
+VALUES(1,'MariaUser','mariaadmin',0),
+(null,'Admin1','administradorincognito',1);
 
 
 
