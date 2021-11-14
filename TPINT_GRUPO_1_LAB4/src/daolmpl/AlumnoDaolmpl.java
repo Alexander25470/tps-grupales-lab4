12d3ec6cf4299362c67338b078363b676bcbf672
Zair <zair@mobilityps.com>
2021-11-14 17:31:16 -0300

    elimina y muestra los que tengan estado 1

--- TPINT_GRUPO_1_LAB4/src/daolmpl/AlumnoDaolmpl.java
+++ TPINT_GRUPO_1_LAB4/src/daolmpl/AlumnoDaolmpl.java
@@ -49,7 +49,7 @@ public class AlumnoDaolmpl implements AlumnoDao {
 		try
 		 {
 			cn.AbrirConexion();
-			 ResultSet rs= cn.query("SELECT alu.*, nac.Nombre as nombreNac, prov.Nombre as nombreProv FROM alumnos alu inner join nacionalidades nac on alu.ID_Nacionalidad = nac.id inner join provincias prov on alu.ID_Provincia = prov.id;");
+			 ResultSet rs= cn.query("SELECT alu.*, nac.Nombre as nombreNac, prov.Nombre as nombreProv FROM alumnos alu inner join nacionalidades nac on alu.ID_Nacionalidad = nac.id inner join provincias prov on alu.ID_Provincia = prov.id where alu.estado != 0;");
 			 while(rs.next())
 			 {
 				 Alumno al = new Alumno();
