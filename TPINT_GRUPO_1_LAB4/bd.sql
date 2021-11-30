f61b3d198e354dc8f1562168cc6cf6ee905aa390
Jogana3 <64289121+Jogana3@users.noreply.github.com>
2021-11-30 18:45:19 -0300

    inserts en alumnos

--- TPINT_GRUPO_1_LAB4/bd.sql
+++ TPINT_GRUPO_1_LAB4/bd.sql
@@ -101,7 +101,8 @@ VALUES('1','Argentina'),('2','Uruguay'),('3','Brasil'),
 insert into PROVINCIAS
 (id,nombre)
 VALUES('1','Jujuy'),('2','Santa Fe'),('3','Cordoba'),
-('4','Chaco'),('5','Misiones'),('6','Santa Cruz'),('7','Entre Rios');
+('4','Chaco'),('5','Misiones'),('6','Santa Cruz'),('7','Entre Rios'),
+('8','Buenos Aires');
 
 insert into LOCALIDADES 
 (id,nombre)
@@ -121,7 +122,17 @@ VALUES('33859632','Maria Perez','1992/10/02','1','3','Av.Libertador 1050','maria
 insert into alumnos
 (dni,nombreApellido,fechaNac,ID_Nacionalidad,ID_Provincia,direccion,email,telefono,estado)
 VALUES('42456321','Lautaro Farias','2000/01/15','2','3','San Martin 1595','lautaro@gmail.com','1178412136',1),
-('40963215','Estela Ricartez','1998/05/15','5','4','Juramento 5050','ricartez@outlook.com','1112457896',1);
+('40963215','Estela Ricartez','1998/05/15','5','4','Juramento 5050','ricartez@outlook.com','1112457896',1),
+('43456989','Julian Alvarez','2000/04/19','1','5','Rivadavia 275','jalvarez@gmail.com','1168340098',1),
+('39784229','Gustavo Garcia','1989/08/20','1','7','Mitre 2867','gus@gmail.com','1190326657',1),
+('39445099','Luciana Suarez','1997/10/25','3','1','Resistencia 2222','lusuarez@outlook.com','1144672323',1),
+('40788213','Yanina Franco','1998/06/04','5','3','DElia 563','yanina@outlook.com','1113249980',1),
+('42908454','Alejo Clifton','2000/10/14','1','8','Alcorta 6743','alejoclifton@gmail.com','113248790',1),
+('42765889','Fernando Moschen','2000/07/02','1','8','27 de Febrero 234','ferchumoschen@gmail.com','1187895543',1),
+('43564987','Nahuel Godoy','2001/09/18','5','6','Alberdi 2291','nahuelgodoy@gmail.com','1177564412',1),
+('43332665','Alexander Popp','2000/02/25','1','4','Agrelo 3211','alexander@gmail.com','11709181234',1),
+('42356777','Zair Cardozo','2001/04/05','1','3','Belgrano 352','zair@outlook.com','1166674413',1),
+('40999876','Franco Coronel','1999/03/12','1','5','Escalada 3388','francoronel@gmail.com','1182761111',1);
 
 insert into estadosalumno
 (id,descripcion)
